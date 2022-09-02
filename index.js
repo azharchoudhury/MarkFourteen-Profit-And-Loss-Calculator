c17e1039d9f5a4cb735f85df63113495893cba5b
azharchoudhury <azharmehmud11@gmail.com>
2022-09-02 16:37:58 +0530

    update: final

--- index.js
+++ index.js
@@ -27,22 +27,27 @@ function calculateProfitAndLoss(initial, qty, current) {
     if (initial === 0 || qty === 0 || current === 0) {
         outputDiv.style.color = "red";
         showOutput("Please enter all the fields");
-    } else if (initial > current) {
-        var loss = ((initial - current) * qty);
-        var lossPercentage = (( (initial - loss)/ initial) * 100);
-        showOutput(`You have incurred a loss of ₹${loss} and the loss percentage is ${lossPercentage.toFixed(2)}%`);
-        outputDiv.style.backgroundColor= "#b91c1c";
-        outputDiv.style.color= "white";
-    } else if (current > initial) {
-        var profit = ((current - initial) * qty);
-        var profitPercentage = (( (current - initial) / initial) * 100);
-        showOutput(`Your have earned a profit of ₹${profit} and the profit percentage is ${profitPercentage.toFixed(2)}%`);
-        outputDiv.style.backgroundColor= "#65a30d";
-        outputDiv.style.color= "white";
     } else {
-        showOutput("No Pain No Gain and, No Gain, So No Pain");
-        outputDiv.style.backgroundColor = "#fcd34d";
-        outputDiv.style.color = "black";
+        if (initial < 0 || qty < 0 || current < 0) {
+            outputDiv.style.color = "red";
+            showOutput("Please enter valid input");
+        } else if (initial > current) {
+            var loss = ((initial - current) * qty);
+            var lossPercentage = (((initial - loss) / initial) * 100);
+            showOutput(`You have incurred a loss of ₹${loss} and the loss percentage is ${lossPercentage.toFixed(2)}%`);
+            outputDiv.style.backgroundColor = "#b91c1c";
+            outputDiv.style.color = "white";
+        } else if (current > initial) {
+            var profit = ((current - initial) * qty);
+            var profitPercentage = (((current - initial) / initial) * 100);
+            showOutput(`Your have earned a profit of ₹${profit} and the profit percentage is ${profitPercentage.toFixed(2)}%`);
+            outputDiv.style.backgroundColor = "#65a30d";
+            outputDiv.style.color = "white";
+        } else {
+            showOutput("No Pain No Gain and, No Gain, So No Pain");
+            outputDiv.style.backgroundColor = "#fcd34d";
+            outputDiv.style.color = "black";
+        }
     }
 }
 
